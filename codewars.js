2a1bc385790a1f7bb87bae842d8e253bf2b4b585
eremitik <39305876+eremitik@users.noreply.github.com>
2021-01-04 11:42:49 +0900

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1974,3 +1974,41 @@ function sumConsecutives(s) {
   }
   return result
 }
+
+
+
+//Detect Pangram (6 kyu)
+function isPangram(string){
+  let count = 0
+  let dict = {
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0,
+    "h": 0,
+    "i": 0,
+    "j": 0,
+    "k": 0,
+    "l": 0,
+    "m": 0,
+    "n": 0,
+    "o": 0,
+    "p": 0,
+    "q": 0,
+    "r": 0,
+    "s": 0,
+    "t": 0,
+    "u": 0,
+    "v": 0,
+    "w": 0,
+    "x": 0,
+    "y": 0,
+    "z": 0,
+  }
+  for (i=0; i<string.length; i++){string[i] === " " || string[i]==="." ? null : dict[string.toLowerCase()[i]]++}
+  for (ltr in dict){dict[ltr]===0 ? count++ : null}
+  return count>0 ? false : true
+  }
